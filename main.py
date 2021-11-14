b590f54785c3f5060ba621da0e91ff310dea0965
Abraham Adeloye <abramadeloye@gmail.com>
2021-11-14 22:07:39 +0000

    added ability to upload pictures. post timeline

--- main.py
+++ main.py
@@ -4,17 +4,15 @@ from flask import Flask, render_template, redirect, url_for, session
 from google.cloud import datastore
 from flask import request
 import flask
-from flask_login import login_user
-from models import user
-from google.cloud import datastore
 import google.cloud.datastore.client
 import requests
 
 import firebase_admin
-from firebase_admin import credentials, storage, auth, db, firestore
-import pyrebase
+from firebase_admin import credentials, auth, firestore
+from google.cloud import ndb
+from google.cloud import storage
 import datetime
-from models.models import User, Relationship, post, postrcv, profile
+from models.models import user, following, followers, post, likes, comments, uposts, comment
 
 config = {
     "apiKey": "AIzaSyATS5J03pAt_LJliBweODydObkCpWTuBJc",
@@ -26,18 +24,23 @@ config = {
     "appId": "1:410902085253:web:0cf2c09d952e7eaae2b3ec",
     "measurementId": "G-QXFW1LCKY3"
 };
+api_key = "AIzaSyATS5J03pAt_LJliBweODydObkCpWTuBJc"
 
 app = Flask(__name__,
             static_folder='/static',
             template_folder='templates')
-app.secret_key = 'super secret key'
+app.secret_key = 'supersecret'
 app.config['SESSION_TYPE'] = 'filesystem'
 credential = credentials.Certificate("keys/firebase.json")
 firebase = firebase_admin.initialize_app(credential)
 
-pb = pyrebase.initialize_app(config)
-bucket = storage.bucket("social-333902.appspot.com")
+storageClient = storage.Client.from_service_account_json('keys/fotoadmin.json')
+bucket = storageClient.get_bucket("foto-334006.appspot.com")
+
 ddb = firestore.client()
+
+ds = google.cloud.ndb.Client.from_service_account_json('keys/fotoadmin.json')
+dsa = datastore.Client.from_service_account_json('keys/fotoadmin.json')
 
 
 @app.route('/signup', methods=['GET', 'POST'])
@@ -54,16 +57,23 @@ def signup():
         try:
             user_record = auth.create_user(email=email, password=password)
             uid = user_record.uid
-            ddb.collection("followers").document(uid).set({'f': []})
-            ddb.collection("following").document(uid).set({'f': []})
-            ddb.collection("uposts").document(uid).set({'f': []})
-            ddb.collection("users").document(uid).set({u"bio": u"",
-                                                       u"followers": ddb.collection("followers").document(uid),
-                                                       u"following": ddb.collection("followings").document(uid),
-                                                       u"posts": ddb.collection("uposts").document(uid),
-                                                       u"ppic": ":/gs",
-                                                       "timestamp": datetime.datetime.now(tz=datetime.timezone.utc),
-                                                       "username": username})
+            with ds.context():  # <- you need this line
+                flr = followers(f=[], id=uid)
+                flr.put()
+                fli = following(f=[], id=uid)
+                fli.put()
+                up = uposts(posts=[], id=uid)
+                up.put()
+                usr = user(email=email,
+                           bio="",
+                           username=username,
+                           followers=flr.key,
+                           following=fli.key,
+                           posts=up.key,
+                           timestamp=datetime.datetime.now(tz=None),
+                           ppic="",
+                           id=uid)
+                usr.put()
             print('Sucessfully created new user: {0}'.format(uid))
             return render_template("signup.html", message="Successfully created user, {user.uid}", action="/signin")
         except Exception as e:
@@ -82,7 +92,7 @@ def signin():
         if email is None or password is None:
             return render_template("signin.html", message="Error missing email or password", action="/signin")
         try:
-            user = pb.auth().sign_in_with_email_and_password(email=email, password=password)
+            user = sign_in_with_email_and_password(email=email, password=password)
             print(user)
             token = user['idToken']
             session['user'] = token
@@ -98,49 +108,66 @@ def signin():
 def home():
     decoded_token = auth.verify_id_token(session['user'])
     uid = decoded_token['uid']
-    posts = ddb.collection('post').stream()
-    postsa = {doc.id: doc.to_dict() for doc in posts}
-    print(postsa)
     reconstructposts = {}
-    for post in postsa:
-        dict = postsa.get(post)
-        author = dict['author'].get().to_dict()['username']
-        comments = {dict['author'].get().to_dict()['username']: dict['comment'] for dict in
-                    list(dict['comments'].get().to_dict()['c'])}
-        likes = 125
-
-        reconstructposts.update({post: {"author": author,
-                                        "comments": comments,
-                                        "timestamp": dict['timestamp'],
-                                        "likes": likes,
-                                        "pic": dict['pic']
-                                        }})
+    with ds.context():
+        posts = post.query()
+        for pos in posts:
+            aut = user.get_by_id(pos.author)
+            coms = {}
+            print(comments.get_by_id(pos.comments.get().key.id()))
+            if comments.get_by_id(pos.comments.get().key.id()) is not None:
+                for com in comments.get_by_id(pos.comments.id()):
+                    comauth = ds
+                    author = aut.username
+                    authorid = aut.id
+                    text = com.comment
+                    tstamp = com.timestamp
+            print(pos.key.id())
+            lks = likes.get_by_id(pos.likes.key.id())
+            print(likes.get_by_id(pos.likes.key.id()))
+            reconstructposts.update({pos: {"author": aut.username,
+                                           "comments": coms,
+                                           "timestamp": pos.timestamp,
+                                           "likes": lks.likecount,
+                                           "pic": pos.pic
+                                           }})
     print(reconstructposts)
 
     return render_template('home.html', posts=reconstructposts)
 
 
 @app.route("/post", methods=['GET', 'POST'])
-def post():
+def p():
     if request.method == "POST":
+        title = request.form['title']
+        file = request.files
+        print(file)
         decoded_token = auth.verify_id_token(session['user'])
         uid = decoded_token['uid']
-        ref = ddb.collection('post').document()
-        rid = ref.id
-        ddb.collection('post').document(rid).set({
-            "author": ddb.collection("users").document(uid),
-            "comments": ddb.collection("comments").document(rid),
-            "likes": ddb.collection("likes").document(rid),
-            "title": "Cool title",
-            "pic": "gs://social-333902.appspot.com/ahmed-saeed-3oY6iqvc2sg-unsplash.jpg",
-            "timestamp": datetime.datetime.now(tz=datetime.timezone.utc)
-        })
-        uposts = ddb.collection("uposts").document(uid).get()
-        uposts['f'].append(ddb.collection('post').document(rid))
-        ddb.collection("uposts").document(uid).update(uposts)
-        ddb.collection("likes").document(rid).set({"a": []})
-        ddb.collection("comments").document(rid).set({"c": []})
-
+        with ds.context():
+            rid = post.allocate_ids(size=1)[0].id()
+            comid = comments.allocate_ids(size=1)[0].id()
+            lid = likes.allocate_ids(size=1)[0].id()
+            print(rid)
+            fLink = uploadFile(file.get('file'), str(rid))
+            com = comments(cms=[], id=comid)
+            lk = likes(likecount=0, likers=[], id=lid)
+            pst = post(author=uid,
+                       comments=ndb.Key(comments, comid),
+                       likes=ndb.Key(likes, lid),
+                       title=title,
+                       pic=fLink,
+                       timestamp=datetime.datetime.now(tz=None),
+                       id=rid)
+            upo = uposts.get_by_id(uid)
+            pos = list(upo.posts)
+            pos.append(ndb.Key(post, rid))
+            upo.posts = pos
+            com.put()
+            lk.put()
+            pst.put()
+            upo.put()
+    return render_template("post.html")
 
 
 @app.errorhandler(500)
@@ -150,6 +177,28 @@ def server_error(e):
     An internal error occurred: <pre>{}</pre>
     See logs for full stacktrace.
     """.format(e), 500
+
+
+def uploadFile(f, id):
+    blob = bucket.blob(id)
+    blob.upload_from_file(f)
+    return blob.public_url
+
+
+def sign_in_with_email_and_password(email, password):
+    request_ref = "https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key={0}".format(api_key)
+    headers = {"content-type": "application/json; charset=UTF-8"}
+    data = json.dumps({"email": email, "password": password, "returnSecureToken": True})
+    request_object = requests.post(request_ref, headers=headers, data=data)
+    raise_detailed_error(request_object)
+    return request_object.json()
+
+
+def raise_detailed_error(request_object):
+    try:
+        request_object.raise_for_status()
+    except requests.HTTPError as e:
+        raise requests.HTTPError(e, request_object.text)
 
 
 if __name__ == '__main__':
--- models/models.py
+++ models/models.py
@@ -6,37 +6,79 @@ from google.cloud import ndb
 
 
 class user(ndb.Model):
-    username = ndb.StringProperty(unique=True)
-    email = ndb.StringProperty(unique=True)
+    """
+    User model
+    Contains username, email, timestamp, followers, following, posts, ppic, and bio.
+    """
+    username = ndb.StringProperty()
+    email = ndb.StringProperty()
     timestamp = ndb.DateTimeProperty(default=datetime.datetime.now)
+    followers = ndb.KeyProperty()
+    following = ndb.KeyProperty()
+    posts = ndb.KeyProperty()
+    ppic = ndb.StringProperty()
+    bio = ndb.StringProperty()
 
 
 class followers(ndb.Model):
+    """
+    Followers model.
+    Contains keys for followers.
+    """
     f = ndb.KeyProperty(repeated=True)
 
 
 class following(ndb.Model):
+    """
+    Following model.
+    Contains keys for following.
+    """
     f = ndb.KeyProperty(repeated=True)
 
 
+class uposts(ndb.Model):
+    """
+    User posts model.
+    Contains keys for a users posts.
+    """
+    posts = ndb.KeyProperty(repeated=True)
+
+
 class post(ndb.Model):
-    timestamp = ndb.StringProperty()
+    """
+    Post model.
+    Contains timestamp, author, photoID, likes, and comments.
+    """
+    title=ndb.StringProperty()
+    timestamp = ndb.DateTimeProperty(default=datetime.datetime.now)
     author = ndb.StringProperty()
-    photoid = ndb.StringProperty()
+    pic = ndb.StringProperty()
     likes = ndb.KeyProperty()
     comments = ndb.KeyProperty()
 
 
 class likes(ndb.Model):
+    """
+    Likes model.
+    Contains likecount and likers.
+    """
     likecount = ndb.IntegerProperty()
     likers = ndb.KeyProperty(repeated=True)
 
 
 class comment(ndb.Model):
+    """
+    Comment model.
+    Contains timestamp, comment, and author.
+    """
     timestamp = ndb.DateTimeProperty()
-    comment = ndb.StringProperty()
+    text = ndb.StringProperty()
     author = ndb.KeyProperty()
 
 
 class comments(ndb.Model):
-    ndb.StructuredProperty(comment, repeated=True)
+    """
+    Comments model.
+    Contains structured property.
+    """
+    cms = ndb.StructuredProperty(comment, repeated=True)
